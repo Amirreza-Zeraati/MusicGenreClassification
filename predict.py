65701199b23c8c635faf806dd145673ebd86fea6
Amirreza Zeraati <iamzeraati@gmail.com>
2023-10-08 22:30:24 +0330

    Update predict.py

--- predict.py
+++ predict.py
@@ -5,25 +5,23 @@ from tensorflow.keras.models import load_model
 
 MODEL_DIR = ''
 AUDIO_PATH = ''
-JSON_PATH = "data/save.json"
 
 N_MFCC = 13
 HOP_LENGTH = 512
 N_FFT = 2048
-SAMPLE_RATE = 22050
+SAMPLE_RATE = 44100
 TRACK_DURATION = 30
+SAMPLES_PER_TRACK = SAMPLE_RATE * TRACK_DURATION
+
 model = load_model(MODEL_DIR)
 
 
 def load_audio(audio_path, num_mfcc=13, n_fft=2048, hop_length=512, num_segments=10):
-    SAMPLE_RATE = 22050
-    TRACK_DURATION = 30
-    SAMPLES_PER_TRACK = SAMPLE_RATE * TRACK_DURATION
     samples_per_segment = int(SAMPLES_PER_TRACK / num_segments)
-    signal, sample_rate = librosa.load(audio_path, sr=SAMPLE_RATE)
+    signal, sr = librosa.load(audio_path, sr=SAMPLE_RATE)
     start = samples_per_segment * 4
     finish = start + samples_per_segment
-    mfcc = librosa.feature.mfcc(y=signal[start:finish], sr=sample_rate, n_mfcc=num_mfcc, n_fft=n_fft,
+    mfcc = librosa.feature.mfcc(y=signal[start:finish], sr=sr, n_mfcc=num_mfcc, n_fft=n_fft,
                                 hop_length=hop_length).T
     return mfcc
 
