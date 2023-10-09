8ae0fa553c7ee1a98efdd817962ad3d35b75ea91
Amirreza Zeraati <iamzeraati@gmail.com>
2023-10-09 18:38:57 +0330

    Update predict.py

--- predict.py
+++ predict.py
@@ -6,17 +6,15 @@ from tensorflow.keras.models import load_model
 MODEL_DIR = ''
 AUDIO_PATH = ''
 
+N_FFT = 2048
 N_MFCC = 13
 HOP_LENGTH = 512
-N_FFT = 2048
 SAMPLE_RATE = 44100
 TRACK_DURATION = 30
 SAMPLES_PER_TRACK = SAMPLE_RATE * TRACK_DURATION
 
-model = load_model(MODEL_DIR)
 
-
-def load_audio(audio_path, num_mfcc=13, n_fft=2048, hop_length=512, num_segments=10):
+def load_audio(audio_path, num_mfcc, n_fft, hop_length, num_segments):
     samples_per_segment = int(SAMPLES_PER_TRACK / num_segments)
     signal, sr = librosa.load(audio_path, sr=SAMPLE_RATE)
     start = samples_per_segment * 4
@@ -26,9 +24,6 @@ def load_audio(audio_path, num_mfcc=13, n_fft=2048, hop_length=512, num_segments
     return mfcc
 
 
-data = load_audio(AUDIO_PATH)
-
-
 def predict(model, X, y):
     X = X[np.newaxis, ...]
     prediction = model.predict(X)
@@ -36,4 +31,6 @@ def predict(model, X, y):
     print("Target: {}, Predicted label: {}".format(y, predicted_index))
 
 
+model = load_model(MODEL_DIR)
+data = load_audio(AUDIO_PATH, N_MFCC, N_FFT, HOP_LENGTH, 10)
 predict(model, data, 6)
